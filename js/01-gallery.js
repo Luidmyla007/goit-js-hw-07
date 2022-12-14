2bf328a5488962f773020082731aed56f3e14770
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-14 22:18:31 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,29 +17,42 @@ const markup = galleryItems
 
 myGalery.insertAdjacentHTML("afterbegin", markup); 
 const galleryItem = document.querySelector(".gallery__image");
-// console.log(galleryItem);
-// console.log(galleryItem.src);
-// console.log(galleryItem.dataset.source);
+console.dir(galleryItem);
+console.log(typeof (galleryItem));
+ console.log(galleryItem);
+console.log(galleryItem.src);
+console.log(galleryItem.dataset.source);
 // const originalPhoto = document.querySelector('[data-source]');
 // console.log(originalPhoto);
 
 
 myGalery.addEventListener('click', onClick);
 function onClick(event) {
-  if (event.target.classList.contains("gallery")) { return; }
-
-  const instance = basicLightbox.create(
-    `<img  class = "original" src="${galleryItem.dataset.source}" alt="${galleryItem.alt}"/>`
-    // const originalPhoto = document.querySelector(".original");
-    // originalPhoto.style.cursor = auto;
-  );
-
+  if (event.target.classList.contains("gallery")) {
+    return;
+  }
+  galleryItem.classList.replace(galleryItem.src, galleryItem.dataset.source);
+  console.log(galleryItem);
   
-  instance.show();
-  instance.close();
-  
-}
+  // const instance = basicLightbox.create(
+    //  galleryItem.src = galleryItem.dataset.source
+    //  galleryItem.classList.replace(galleryItem.src, galleryItem.dataset.source)
 
+    // `<img  class = "original" src="${galleryItem.dataset.source}" alt="${galleryItem.alt}"/>`
+     
+  
+    
+  
+      // const originalPhoto = document.querySelector(".original");
+      // originalPhoto.style.cursor = auto;
+    // );
+  
+  // const originalPhoto = document.querySelector(".original");
+  // originalPhoto.style.cursor = auto;
+  
+  // instance.show(); 
+ }
+//  instance.close(); 
 
 // const instance = basicLightbox.create(`
 // 	<h1>Dynamic Content</h1>
